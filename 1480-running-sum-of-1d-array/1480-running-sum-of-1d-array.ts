3707d094d5e84b64609e1fee68fb0dd8b1e3878c
Scott Obert <13769142+scottobert@users.noreply.github.com>
2022-09-19 11:22:04 -0700

    Time: 109 ms (42.20%), Space: 44.7 MB (58.34%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.ts
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.ts
@@ -1,8 +1,6 @@
 function runningSum(nums: number[]): number[] {
-    const result = [];
-    result[0] = nums[0];
     for(let i = 1; i < nums.length; i++) {
-        result[i] = result[i-1] + nums[i];
+        nums[i] = nums[i-1] + nums[i];
     }
-    return result;
+    return nums;
 };
